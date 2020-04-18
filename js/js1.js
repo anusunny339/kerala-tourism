8316ff73a6ab75698f53b5d1a0550f7483948b11
anusunny339 <anusunny339@gmail.com>
2020-04-18 20:08:42 +0530

    changes in validation

--- js/js1.js
+++ js/js1.js
@@ -8,63 +8,78 @@ function log()
 {
  
 let regexp=/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+var phone=document.getElementById("phone");
+var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+
 
     if(regexp.test(username.value)){
         error1.innerHTML="valid email id";
         error1.style.color="green";
-   
+        username.style.border="2px solid rgb(43, 107, 226)";
+        
+        if(phone.value.match(phoneno))
+        {
+          error3.innerHTML="valid phone no";
+          error3.style.color="green";
+          phone.style.border="2px solid rgb(43, 107, 226)";
         if(password.value.length>=5)
         {
        
-         if(password.value != cpassword.value)
-          {
-            alert("Error: Password and confirm password not match!");
-        
-             return false;
-           }
-    
           if(password.value == username.value) {
           alert("Error: Password must be different from Username!");
-        
+          password.focus();
+          password.style.border="2px solid red";
            return false;
           }
 
           re = /[0-9]/;
           if(!re.test(password.value)) {
            alert("Error: password must contain at least one number (0-9)!");
-        
+           password.focus();
+           password.style.border="2px solid red";
            return false;
           }
           re = /[a-z]/;
           if(!re.test(password.value)) {
           alert("Error: password must contain at least one lowercase letter (a-z)!");
-        
+          password.focus();
+          password.style.border="2px solid red";
           return false;
          }
           re = /[A-Z]/;
          if(!re.test(password.value)) {
          alert("Error: password must contain at least one uppercase letter (A-Z)!");
-        
+         password.focus();
+         password.style.border="2px solid red";
           return false;
       
          } 
+         if(password.value != cpassword.value)
+          {
+            alert("Error: Password and confirm password not match!");
+            cpassword.focus();
+            cpassword.style.border="2px solid red";
+             return false;
+           }
+      
+           password.style.border="2px solid rgb(43, 107, 226)";
+
       error2.innerHTML="password  is strong";
       error2.style.color="green";
+      password.style.border="2px solid rgb(43, 107, 226)";
       return true;
      
     }
     else{
     
     
-      if(password.value !=  cpassword.value)
-      {
-        alert("Error: Password and confirm password not match!");
-        
-        return false;
-      }
+     
     
       if(password.value == username.value) {
         alert("Error: Password must be different from Username!");
+        password.focus();
+        password.style.border="2px solid red";
         return false;
         
       }
@@ -72,156 +87,55 @@ let regexp=/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
       re = /[0-9]/;
       if(!re.test(password.value)) {
         alert("Error: password must contain at least one number (0-9)!");
+        password.focus();
+        password.style.border="2px solid red";
         return false;
         
       }
       re = /[a-z]/;
       if(!re.test(password.value)) {
         alert("Error: password must contain at least one lowercase letter (a-z)!");
-        
+        password.focus();
+        password.style.border="2px solid red";
         return false;
       }
       re = /[A-Z]/;
       if(!re.test(password.value)) {
         alert("Error: password must contain at least one uppercase letter (A-Z)!");
+        password.focus();
+        password.style.border="2px solid red";
         return false;
-        
-      
+       
       } 
+      if(password.value !=  cpassword.value)
+      {
+        alert("Error: Password and confirm password not match!");
+        cpassword.focus();
+        Cpassword.style.border="2px solid red";
+        return false;
+      }
       error2.innerHTML="password  is weak";
       error2.style.color="red";
        password.style.border="2px solid red";
       return true;
 
     }  
+  }
+ //phone else{}
+ else{
+   error3.innerHTML="invalid phone no";
+   error3.style.color="red";
+   phone.style.border="2px solid red";
+   phone.focus();
+   return false;
+ }
 }
  else{
     error1.innerHTML="Invalid email id";
     error1.style.color="red";
-    
+    username.style.border="2px solid red";
+    username.focus();
     return false;
  }
 }
 
-function passcheck()
-{
-
-    if(password.value.length>=5)
-    {
-    
-    if(password.value == username.value) {
-        alert("Error: Password must be different from Username!");
-        password.focus();
-        return false;
-      }
-
-      re = /[0-9]/;
-      if(!re.test(password.value)) {
-        alert("Error: password must contain at least one number (0-9)!");
-        password.focus();
-        return false;
-      }
-      re = /[a-z]/;
-      if(!re.test(password.value)) {
-        alert("Error: password must contain at least one lowercase letter (a-z)!");
-        password.focus();
-        return false;
-      }
-      re = /[A-Z]/;
-      if(!re.test(password.value)) {
-        alert("Error: password must contain at least one uppercase letter (A-Z)!");
-        password.focus();
-        return false;
-       
-      } 
-      error2.innerHTML="password  is strong";
-      error2.style.color="green";
-      return true;
-   
-    }
-   else{
-    
-    
-      if(password.value == username.value) {
-        alert("Error: Password must be different from Username!");
-        password.focus();
-        return false;
-        
-      }
-
-      re = /[0-9]/;
-      if(!re.test(password.value)) {
-        alert("Error: password must contain at least one number (0-9)!");
-        password.focus();
-        return false;
-        
-      }
-      re = /[a-z]/;
-      if(!re.test(password.value)) {
-        alert("Error: password must contain at least one lowercase letter (a-z)!");
-        password.focus();
-        return false;
-      }
-      re = /[A-Z]/;
-      if(!re.test(password.value)) {
-        alert("Error: password must contain at least one uppercase letter (A-Z)!");
-        password.focus();
-        return false;
-        
-      
-      } 
-     error2.innerHTML="password  is weak";
-     error2.style.color="red";
-     password.style.border="2px solid red";
-     return true;
-
-    }  
-}
-
-
-function cpasscheck()
-{
-    if(password.value != cpassword.value)
-    {
-        alert("Error: Password and confirm password not match!");
-        cpassword.value="";
-        cpassword.focus();
-        return false;
-    }  
-}
-
-function checkemail()
-{
-    let regexp=/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-
-
- 
- if(regexp.test(username.value)){
-    error1.innerHTML="valid email id";
-    error1.style.color="green";
-   
- }
- else{
-    error1.innerHTML="Invalid email id";
-    error1.style.color="red";
-    
-    username.focus();
- }
-}
-
-function phonecheck()
-{
-    var phone=document.getElementById("phone");
-    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    
-    if(phone.value.match(phoneno))
-        {
-       return true;
-        }
-      else
-        {
-        alert("invalid phone no");
-        return false;
-        phone.focus();
-        }
-}
---
+++ js/login.js
@@ -0,0 +1,102 @@
+function login()
+{
+ var username=document.getElementById("email");
+ var password=document.getElementById("password");
+ let regexp=/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    if(regexp.test(username.value)){
+        error1.innerHTML="valid email id";
+        error1.style.color="green";
+        username.style.border="2px solid rgb(43, 107, 226)";
+        if(password.value.length>=5)
+        {
+       
+        if(password.value == username.value) {
+            alert("Error: Password must be different from Username!");
+            password.focus();
+            password.style.border="2px solid red";
+             return false;
+            }
+  
+            re = /[0-9]/;
+            if(!re.test(password.value)) {
+             alert("Error: password must contain at least one number (0-9)!");
+             password.focus();
+             password.style.border="2px solid red";
+             return false;
+            }
+            re = /[a-z]/;
+            if(!re.test(password.value)) {
+            alert("Error: password must contain at least one lowercase letter (a-z)!");
+            password.focus();
+            password.style.border="2px solid red";
+            return false;
+           }
+            re = /[A-Z]/;
+           if(!re.test(password.value)) {
+           alert("Error: password must contain at least one uppercase letter (A-Z)!");
+           password.focus();
+           password.style.border="2px solid red";
+            return false;
+        
+           } 
+           
+             password.style.border="2px solid rgb(43, 107, 226)";
+    
+        password.style.border="2px solid rgb(43, 107, 226)";
+        return true;
+       
+      }
+      else{
+      
+      
+       
+      
+        if(password.value == username.value) {
+          alert("Error: Password must be different from Username!");
+          password.focus();
+          password.style.border="2px solid red";
+          return false;
+          
+        }
+  
+        re = /[0-9]/;
+        if(!re.test(password.value)) {
+          alert("Error: password must contain at least one number (0-9)!");
+          password.focus();
+          password.style.border="2px solid red";
+          return false;
+          
+        }
+        re = /[a-z]/;
+        if(!re.test(password.value)) {
+          alert("Error: password must contain at least one lowercase letter (a-z)!");
+          password.focus();
+          password.style.border="2px solid red";
+          return false;
+        }
+        re = /[A-Z]/;
+        if(!re.test(password.value)) {
+          alert("Error: password must contain at least one uppercase letter (A-Z)!");
+          password.focus();
+          password.style.border="2px solid red";
+          return false;
+         
+        } 
+        
+        
+  
+      } 
+      password.style.border="2px solid red";
+        return true; 
+    }
+   
+   else{
+      error1.innerHTML="Invalid email id";
+      error1.style.color="red";
+      username.style.border="2px solid red";
+      username.focus();
+      return false;
+   }
+   
+}
